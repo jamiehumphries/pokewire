ecddaf3f6ca138138f223b92fb23d475fb69a917
Jamie Humphries <jamie.humphries@softwire.com>
2020-06-15 17:08:52 +0100

    Save Ditto for special spawn handling.

--- spawns.js
+++ spawns.js
@@ -7,6 +7,10 @@ const SHINY_PROBABILITY = +process.env.SHINY_PROBABILITY || 0.02
 
 function randomSpawn (maxID) {
   const id = Math.floor(Math.random() * maxID + 1)
+  if (id === 132) {
+    // Re-roll if Ditto until special handling is in place.
+    return randomSpawn(maxID)
+  }
   const name = pkm.getName(id)
   const gender = randomGender(id)
   const shiny = Math.random() < SHINY_PROBABILITY
