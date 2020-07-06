bd6cb50330e653434076120472800374519bd9ee
Jamie Humphries <jamie.humphries@softwire.com>
2020-07-06 15:27:44 +0100

    Fix Unova / Kalos boundary.

--- generations.js
+++ generations.js
@@ -8,8 +8,8 @@ module.exports = Object.freeze([
   { name: 'Johto', minId: 152, maxId: 251 },
   { name: 'Hoenn', minId: 252, maxId: 386 },
   { name: 'Sinnoh', minId: 387, maxId: 493 },
-  { name: 'Unova', minId: 494, maxId: 648 },
-  { name: 'Kalos', minId: 649, maxId: 721 },
+  { name: 'Unova', minId: 494, maxId: 649 },
+  { name: 'Kalos', minId: 650, maxId: 721 },
   { name: 'Alola', minId: 722, maxId: 807 },
   { name: 'Unknown', minId: 808, maxId: 809 },
   { name: 'Galar', minId: 810, maxId: 890 }
