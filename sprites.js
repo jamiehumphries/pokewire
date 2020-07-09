3867acdf0c9282e29273b68b093be78d4c12cf8b
Jamie Humphries <jamie.humphries@softwire.com>
2020-07-09 11:30:14 +0100

    Temporary special case for Unown.

--- sprites.js
+++ sprites.js
@@ -14,9 +14,17 @@ function getSprite (spawn) {
   if (spawn.shiny) {
     spriteParts.push('shiny')
   }
-  spriteParts.push(spawn.id)
+  let filename = spawn.id.toString()
+  if (spawn.id === 201) {
+    // Special case for Unown.
+    const forms = 'abcdefghijklmnopqrstuvwxyz'
+    const form = forms[Math.floor(Math.random() * 26)]
+    filename += `-${form}`
+  }
+  filename += '.png'
+  spriteParts.push(filename)
   const sprite = spriteParts.join('/')
-  return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${sprite}.png`
+  return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${sprite}`
 }
 
 module.exports = {
