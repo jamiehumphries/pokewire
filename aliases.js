dbbffeecec7fa5dd9d7c439bd298e19fe4003130
Jamie Humphries <jamie.humphries@softwire.com>
2020-07-17 16:50:22 +0100

    Add more aliases.

--- aliases.js
+++ aliases.js
@@ -4,7 +4,14 @@ const aliases = {
    29: ['Nidoran'],
    32: ['Nidoran'],
    83: ['Farfetch\'d', 'Farfetchd'],
-  122: ['Mr Mime']
+  122: ['Mr Mime'],
+  250: ['HoOh', 'Ho Oh'],
+  439: ['Mime Jr'],
+  669: ['Flabébé', 'Flabébe', 'Flabebé'],
+  772: ['Type Null'],
+  782: ['Jangmoo', 'Jangmo o'],
+  783: ['Hakamoo', 'Hakamo o'],
+  784: ['Kommoo', 'Kommo o']
 }
 /* eslint-enable indent */
 
